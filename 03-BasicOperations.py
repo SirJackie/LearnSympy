45988013ab12a483d1047854958a5ceb77540008
lyj00912 <3122742760@qq.com>
2022-01-15 00:29:11 +0800

    tried substitution and sp.sympify()

---
+++ 03-BasicOperations.py
@@ -0,0 +1,23 @@
+import sympy as sp
+
+# # Substitution
+# # Tip: expr.subs() returns a new expression. SymPy objects are immutable.
+# # That means that subs does not modify it in-place.
+# # The creating process of expr2 does not modify expr1
+# x = sp.symbols("x")
+# expr1 = sp.cos(x) + sp.Integer(1)
+# expr2 = expr1.subs(x, 0)
+# print(expr1)
+# print(expr2)
+
+# # Multiple Substitution
+# x, y, z = sp.symbols("x y z")
+# expr1 = x**3 + 4*x*y - z
+# expr2 = expr1.subs([(x, 2), (y, 4), (z, 0)])
+# print(expr1)
+# print(expr2)
+
+# SYMPY-fy AKA sp.sympify()
+strExpr = "(x + 1) ** 2"
+expr = sp.sympify(strExpr)
+print(expr)

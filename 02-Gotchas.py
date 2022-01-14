95a0e81ee06b2ccc4eba0876289983b487c00fb4
lyj00912 <3122742760@qq.com>
2022-01-14 23:55:37 +0800

    tried check whether two numbers are equals

--- 02-Gotchas.py
+++ 02-Gotchas.py
@@ -1,7 +1,28 @@
 import sympy as sp
 
+# # Symbol's Unchangeable Feature
+# x = sp.symbols("x")
+# expr = x + 1
+# # x = 2  # This won't change expr
+# print(expr)
+# expr = expr.subs(x, 2)  # subs means substitute
+# print(expr)
+
+# Eq to create symbolic equalities
+# x = sp.symbols("x")
+# expr = sp.Eq(x + 1, 4)
+# print(expr)
+
+# Check whether a == b
 x = sp.symbols("x")
-expr = x + 1
-print(expr)
-expr = expr.subs(x, 2)  # subs means substitute
-print(expr)
+expr1 = (x + 1) ** 2
+expr2 = x**2 + 2*x + 1
+print(expr1, "---;---", expr2)
+
+# Method 1: Just check if a-b=0, need to use sp.simplify()
+expr3 = expr1 - expr2
+expr4 = sp.simplify(expr3)
+print(expr4)
+
+# Method 2: expr.equals(expr)
+print(expr3.equals(expr4))
